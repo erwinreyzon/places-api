920e3f39edf4e130bebead36fc0f65dfb356fc18
erwin <winzon353535@gmail.com>
2022-04-12 14:07:32 -0700

    adds update route and action for place

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/places" => "places#index"
   get "/places/:id" => "places#show"
   post "/places" => "places#create"
+  patch "/places/:id" => "places#update"
 
 end
