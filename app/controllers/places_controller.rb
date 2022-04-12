0d8ec65166e89e8dd9bdebb86b925db6e50cd687
erwin <winzon353535@gmail.com>
2022-04-12 14:19:41 -0700

    update place index for jb view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,8 @@
 class PlacesController < ApplicationController
 
   def index
-    places = Place.all
-    render json: places
+    @places = Place.all
+    render template: "places/index"
   end
 
   def show
